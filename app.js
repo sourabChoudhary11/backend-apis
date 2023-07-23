a27ec6d5c5471974045e81325d209b008395d63e
sourabChoudhary11 <123931473+sourabChoudhary11@users.noreply.github.com>
2023-07-23 19:39:02 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ import cookieParser from "cookie-parser";
 import router from "./routes/route.js";
 import {taskRouter} from "./routes/task.js";
 import { errorMiddleware } from "./middlewares/error.js";
+import cors from "cors";
 
 export const app = express();
 
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors({
+   origin: [process.env.FRONTED_URI],
+   methods: ["POST","GET", "PUT", "DELETE"],
+   credentials:true
+}))
 app.use("/api/v1/tasks", taskRouter)
 app.use("/api/v1/users/", router)
 
--- utils/features.js
+++ utils/features.js
@@ -4,7 +4,9 @@ export const setCookie = async (message, status, user, res)=>{
     const token = await jwt.sign({ _id: user._id }, process.env.TOKE_SECRET);
     res.status(status).cookie("token", token, {
         httpOnly: true,
-        maxAge: 15 * 60 * 1000
+        maxAge: 15 * 60 * 1000,
+        sameSite: "none",
+        secure: true,
     }).json({
         status: true,
         message
